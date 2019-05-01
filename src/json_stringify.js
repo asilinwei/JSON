0ab0524ebaf038a3f9169043ea03bc78fd3604b4
asilinwei <1021069119@qq.com>
2019-05-01 16:47:11 +0800

    :

--- src/json_stringify.js
+++ src/json_stringify.js
@@ -81,8 +81,11 @@ var json_stringify = (function() {
     if (isRegExp(value)) {
       return '{}';
     }
-    if (typeof value === 'string' || isStringObject(value) || isDate(value)) {
-      return '"' + (isDate(value) ? value.toISOString() : value) + '"';
+    if (typeof value === 'string' || isStringObject(value)) {
+      return '"' + value.replace
+    }
+    if (isDate(value)) {
+      return '"' + value.toISOString() + '"';
     }
     if (isArray(value)) {
       return array(value, indent);
